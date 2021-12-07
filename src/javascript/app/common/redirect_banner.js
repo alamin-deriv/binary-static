dfadd697449443d7452b586cd12ca24b280d6458
alamin-deriv <alamin@deriv.com>
2021-12-07 10:50:41 +0100

    handle redirect before login

--- src/javascript/app/common/redirect_banner.js
+++ src/javascript/app/common/redirect_banner.js
@@ -1,28 +1,17 @@
 const DerivBanner = require('./deriv_banner');
-const getElementById = require('../../_common/common_functions').getElementById;
 const BinarySocket = require('../base/socket');
-const getLanguage       = require('../../_common/language').get;
 const State = require('../../_common/storage').State;
 const Client = require('../base/client');
 const isEuCountrySelected      = require('../../_common/utility').isEuCountrySelected;
 
 const RedirectBanner = (() => {
 
-    let el_redirect_banner_container, el_redirect_link;
-
     const onLoad = () => {
         BinarySocket.wait('authorize', 'website_status', 'landing_company').then(() => {
 
             const eu_country = isEuCountrySelected(Client.get('residence')) || isEuCountrySelected(State.getResponse('website_status.clients_country'));
             if (eu_country) {
-
-                el_redirect_banner_container = getElementById('redirect_banner_container');
-                el_redirect_link = getElementById('redirect-link');
-                el_redirect_banner_container.setVisibility(1);
-                const lang = getLanguage().toLowerCase();
-                const multiplier_href = `https://deriv.com/${lang}/`;
-
-                el_redirect_link.href = multiplier_href;
+                handleRedirect();
             }
             
         });
